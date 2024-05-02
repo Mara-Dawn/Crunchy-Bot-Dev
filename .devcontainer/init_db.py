ec3e8e30b1c8539922fd55ae275bc0297ea47c39
Mara Dawn <MaraDawn69@Gmail.com>
2024-05-02 18:43:02 +0200

    added seasonal db support

--- .devcontainer/init_db.py
+++ .devcontainer/init_db.py
@@ -4,11 +4,14 @@ from sqlite3 import Error
 conn = None
 try:
     conn = sqlite3.connect("database.sqlite")
+    conn_core = sqlite3.connect("core.sqlite")
 except Error as e:
     print(e)
 finally:
     if conn:
         conn.close()
+    if conn_core:
+        conn_core.close()
 
 files = ["key.txt", "tenor.txt", "openai.txt"]
 for file in files:
