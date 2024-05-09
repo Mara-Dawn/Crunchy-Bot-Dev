3a68e6f102916c8adb95326264aad41f53c82983
Mara Dawn <MaraDawn69@Gmail.com>
2024-05-09 16:11:40 +0200

    seasonal db no longer needed

--- .devcontainer/init_db.py
+++ .devcontainer/init_db.py
@@ -3,15 +3,12 @@ from sqlite3 import Error
 
 conn = None
 try:
-    conn = sqlite3.connect("season.sqlite")
-    conn_core = sqlite3.connect("database.sqlite")
+    conn = sqlite3.connect("database.sqlite")
 except Error as e:
     print(e)
 finally:
     if conn:
         conn.close()
-    if conn_core:
-        conn_core.close()
 
 files = ["key.txt", "tenor.txt", "openai.txt"]
 for file in files:
